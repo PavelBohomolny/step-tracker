0e20a68382615f3c46f0c2a590b80c98eeffcfd3
Pavel Bohomolnyi <pavel.bohomolnyi@skopei.com>
2024-04-30 15:32:28 +0200

    created xcode project

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Pavel Bohomolnyi on 30/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
