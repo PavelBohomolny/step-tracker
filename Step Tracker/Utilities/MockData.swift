a14a9c8fa7decbd03f918b4a73e938007cc1c235
Pavel Bohomolnyi <pbogomolny93@gmail.com>
2024-05-27 16:01:58 +0200

    weight diff bar chart

--- Step Tracker/Utilities/MockData.swift
+++ Step Tracker/Utilities/MockData.swift
@@ -13,11 +13,7 @@ struct MockData {
 
         for i in 0 ..< 28 {
             let metric = HealthMetric(
-                date: Calendar.current.date(
-                    byAdding: .day,
-                    value: -i,
-                    to: .now
-                )!,
+                date: Calendar.current.date(byAdding: .day, value: -i, to: .now)!,
                 value: .random(in: 4_000...15_000)
             )
             array.append(metric)
@@ -25,20 +21,30 @@ struct MockData {
 
         return array
     }
-    
+
     static var weights: [HealthMetric] {
         var array: [HealthMetric] = []
 
         for i in 0 ..< 28 {
             let metric = HealthMetric(
-                date: Calendar.current.date(
-                    byAdding: .day,
-                    value: -i,
-                    to: .now
-                )!,
+                date: Calendar.current.date(byAdding: .day, value: -i, to: .now)!,
                 value: .random(in: (160 + Double(i/3)...165 + Double(i/3)))
             )
             array.append(metric)
+        }
+
+        return array
+    }
+
+    static var weightDiffs: [WeekdayChartData] {
+        var array: [WeekdayChartData] = []
+
+        for i in 0 ..< 7 {
+            let diff = WeekdayChartData(
+                date: Calendar.current.date(byAdding: .day, value: -i, to: .now)!,
+                value: .random(in: -3...3)
+            )
+            array.append(diff)
         }
 
         return array
